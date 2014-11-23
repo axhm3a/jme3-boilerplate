aea7bef5b18c2dfe0110ccce431074b109b97004
axhm3a <axhm3a@gmail.com>
2014-11-23 19:33:20 +0100

    initial commit

---
+++ core/src/main/java/org/axhm3a/jme3boilerplate/Main.java
@@ -0,0 +1,12 @@
+package org.axhm3a.jme3boilerplate;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ * Created by axhm3a on 23.11.14.
+ */
+public class Main extends SimpleApplication {
+    @Override
+    public void simpleInitApp() {
+    }
+}
---
+++ desktop/src/main/java/org/axhm3a/jm3boilerplate/DesktopMain.java
@@ -0,0 +1,14 @@
+package org.axhm3a.jm3boilerplate;
+
+import com.jme3.app.SimpleApplication;
+import org.axhm3a.jme3boilerplate.Main;
+
+/**
+ * Created by axhm3a on 23.11.14.
+ */
+public class DesktopMain {
+    public static void main(String[] argv) {
+        SimpleApplication simpleApplication = new Main();
+        simpleApplication.start();
+    }
+}
